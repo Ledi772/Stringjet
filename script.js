2dc0f6bd14e078bef950efb215c791ad018290a8
Ledi772 <halilijuled2@gmail.com>
2024-12-10 07:52:17 -0800

    e kryva

---
+++ script.js
@@ -0,0 +1,24 @@
+/* per derty duhesh mi pas tri varibla njonen njesia vlera edhe njo mi kthy duhesh me formulen e trye mi kthy edhe duhesh  */
+/* let stringu1 = "Mirjeta ndiaed"
+console.log(stringu1.length);
+let indeksiFundit = stringu1.length -1
+console.log(stringu1[indeksiFundit]);  */
+
+let fjala = "abcd"
+
+//fjala[0] = a;
+//fjala[1] = b;
+//fjala[2] = c;
+//fjala[3] = d;
+//console.log(fjala length)
+
+for (let i=0; i < fjala.length; i++){
+    console.log("fjala[" +i+ "]) = ' + fjala[i]")
+};
+
+// console.log(fjala[0] = a;)
+// console.log(fjala[1] = b;)
+// console.log(fjala[2] = c;)
+// console.log(fjala[3] = d;)
+
+
